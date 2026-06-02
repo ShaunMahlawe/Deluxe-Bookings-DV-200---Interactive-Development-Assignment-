7e967e363ccc7277d52d46835f229f1d6cd62e8f
Khanyisile_251301 <251301@virtualwindow.co.za>
2026-06-02 18:34:09 +0200

    hotelcarousel file added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css';
 
 import ReviewForm from "./components/ReviewForm";
+import HotelCarousel from "./components/HotelCarousel";
 
 function App() {
   return (
-    <>
-      <ReviewForm />
-    </>
+    <div className="App">
+      <section className="review-section">
+        <ReviewForm />
+      </section>
+
+      <section className="hotelcarousel-section">
+        <HotelCarousel />
+      </section>
+    </div>
   );
 }
 
