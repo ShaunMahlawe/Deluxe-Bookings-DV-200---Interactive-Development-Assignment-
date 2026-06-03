214462c12a76bb66bdc18516b2a2a9c55e6640a9
Khanyisile_251301 <251301@virtualwindow.co.za>
2026-06-03 22:41:35 +0200

    files moved

--- mern-frontend/src/pages/public/home.jsx
+++ mern-frontend/src/pages/public/home.jsx
@@ -2,11 +2,21 @@ import { Nav } from "react-bootstrap";
 import HeroHeader from "../../components/common/heroheader";
 import Footer from "../../components/common/footer";
 import NavBar from "../../components/common/navbar";
+import HotelCarousel from "../Components/HotelCarousel";
+import AboutSection from "../Components/AboutSection";
+import LuxurySection from "../Components/LuxurySection";
+import LuxuryCarousel from "../Components/LuxuryCarousel";
+import OffersSection from "../Components/OfferSection";
 
 function Home() {
   return (
     <div>
   <HeroHeader></HeroHeader>
+  <HotelCarousel></HotelCarousel>
+  <AboutSection></AboutSection>
+  <LuxurySection></LuxurySection>
+  <OffersSection></OffersSection>
+  <LuxuryCarousel></LuxuryCarousel>
   <Footer></Footer>
   </div>
   )
